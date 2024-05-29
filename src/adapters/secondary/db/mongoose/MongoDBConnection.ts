3ef438a4945ce25971a53d2145e2c6d4d0c7c339
Sajjad <sajjadhossainsb01@gmail.com>
2024-05-30 01:05:05 +0600

    mongodb connection issue fixed

--- src/adapters/secondary/db/mongoose/MongoDBConnection.ts
+++ src/adapters/secondary/db/mongoose/MongoDBConnection.ts
@@ -3,6 +3,7 @@ import { config } from '../../../../config/config';
 
 export const connectToMongoDB = async () => {
   try {
+    console.log(config.mongodbUri);
     await mongoose.connect(config.mongodbUri);
     console.log('MongoDB connected');
   } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { connectToMongoDB } from './adapters/secondary/db/mongoose/MongoDBConnection';
 import { config } from './config/config';
-// import userRouter from "./adapters/primary/http/routes/userRoutes"
+import userRouter from "./adapters/primary/http/routes/userRoutes"
 
 const app = express();
 
 app.use(express.json());
 
-// const prefix = '/api/v1'
-// app.use(`${prefix}/users`, userRouter)
+const prefix = '/api/v1'
+app.use(`${prefix}/users`, userRouter)
 
 const startServer = async () => {
   await connectToMongoDB();
