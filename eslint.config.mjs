32bdbaa80b7ca23fb8a9db947ed1d6b8ef0273bf
Sajjad <sajjadhossainsb01@gmail.com>
2024-05-29 18:07:06 +0600

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/ban-types": [
+        "error",
+        {
+          types: {
+            any: {
+              message: "Use a more specific type instead of `any`.",
+            },
+            unknown: {
+              message: "Use a more specific type instead of `unknown`.",
+            },
+          },
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: ["node_modules/", "dist/", "build/"],
+  },
+];
---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+interface Config {
+    port: string | number;
+    mongodbUri: string;
+  }
+
+export const config:Config = {
+  port: process.env.PORT || 3000,
+  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydb',
+};
