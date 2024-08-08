8b5f9ce64262f03df6def4615cdfbcb0da385ede
Sajjad Hossain <sajjadhossain@Sajjads-Laptop.local>
2024-08-08 11:02:22 +0600

    added cl for check pull request

--- src/core/ports/AuthorRepositoryPort.ts
+++ src/core/ports/AuthorRepositoryPort.ts
@@ -2,7 +2,7 @@
 import { PaginatedAuthors } from '../../types/dtos/AuthorDTO'
 import { CreateAuthorRequest } from '../../types/requests/author/CreateAuthorRequest'
 import { Author } from '../domain/entities/Author'
-
+console.log('hello check pull request');
 export interface AuthorRepositoryPort {
   createAuthor(request: CreateAuthorRequest): Promise<Author>
   getAuthorById(id: string): Promise<Author | null>
