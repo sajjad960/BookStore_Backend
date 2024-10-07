4067eae34757ae8d5885c75eae434e4afe4a23fe
sajjad <sajjad@elobyte.dev>
2024-10-07 18:56:07 +0600

    changed position of test

--- src/tests/integration/userIntegration.test.ts
+++ src/tests/integration/userIntegration.test.ts
@@ -4,7 +4,8 @@ import { app } from '../..'
 describe('User Integration Tests', () => {
   it('should create a user', async () => {
     const response = await request(app).get('/api/v1/books')
-    expect(response.status).toBe(200)
+    // expect(response.status).toBe(200)
     // expect(response.body.data.user).toHaveProperty('id')
+    expect(response.status).toBe(200)
   })
 })
